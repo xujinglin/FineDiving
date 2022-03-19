cd32488e1206ba9d92ba79b4e833f8176bffd1d3
Jinglin Xu <xujinglin_study@163.com>
2022-03-19 22:27:13 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-mkdir -p logs
-now=$(date +"%m%d_%H%M")
-log_name="LOG_Test_$2_$1_$now"
-CUDA_VISIBLE_DEVICES=0,1 python3 -u main.py --archs $1 --benchmark $2 --test $4 2>&1|tee logs/$log_name.log
-
-# bash test.sh TSA FineDiving 0,1 --ckpts ./pretrained-model.pth
